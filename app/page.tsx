914928956b469c73567c4b15e8cd3076849f47bf
rizkianakbar <rizkianakbar4@gmail.com>
2024-05-20 07:53:22 +0700

    feat: add text to speech logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,150 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState, useEffect } from "react";
+import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+const getVoiceId = (voice: SpeechSynthesisVoice) =>
+  `${voice.name}-${voice.lang}`;
+
+export default function Page() {
+  const [text, setText] = useState("");
+  const [rate, setRate] = useState<number>(1);
+  const [pitch, setPitch] = useState<number>(1);
+  const [selectedVoiceId, setSelectedVoiceId] = useState<string | null>(null);
+  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
+  const [synth, setSynth] = useState<SpeechSynthesis | null>(null);
+  const [isSpeaking, setIsSpeaking] = useState<boolean>(false);
+
+  useEffect(() => {
+    const synthesis = window.speechSynthesis;
+    setSynth(synthesis);
+
+    const getVoices = () => {
+      const voicesList: SpeechSynthesisVoice[] = synthesis.getVoices();
+      setVoices(voicesList);
+    };
+
+    getVoices();
+
+    if (synthesis.onvoiceschanged !== undefined) {
+      synthesis.onvoiceschanged = getVoices;
+    }
+  }, []);
+
+  const selectedVoice = selectedVoiceId
+    ? voices.find((voice) => getVoiceId(voice) === selectedVoiceId)
+    : null;
+
+  const speak = () => {
+    if (synth && synth.speaking) {
+      console.error("Already speaking...");
+      return;
+    }
+
+    if (text !== "" && synth && selectedVoice) {
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.onend = () => {
+        console.log("Speech ended");
+        setIsSpeaking(false);
+      };
+      utterance.onerror = (event) => {
+        console.error(`Speech synthesis error: ${event.error}`);
+        setIsSpeaking(false);
+      };
+      utterance.voice = selectedVoice;
+      utterance.rate = rate;
+      utterance.pitch = pitch;
+      synth.speak(utterance);
+      setIsSpeaking(true);
+    }
+  };
+
+  const stop = () => {
+    if (synth && synth.speaking) {
+      synth.cancel();
+      setIsSpeaking(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="flex flex-col items-center h-screen max-w-lg gap-8 px-4 py-10 mx-auto md:px-0">
+      <h1 className="text-3xl font-bold">Text to Speech</h1>
+      <p className="text-center">
+        Welcome to our Text to Speech application! Here, you can input any text
+        you&apos;d like and play it back as speech. You also have the option to
+        customize the speech by adjusting the rate and pitch to suit your
+        preference.
+      </p>
+      <Textarea
+        placeholder="Type your text here..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <div className="flex flex-col w-full gap-4">
+        <Label htmlFor="rate" className="flex items-center justify-between">
+          <div>Speech Rate</div>
+          <div>{rate}x</div>
+        </Label>
+        <Slider
+          max={200}
+          step={1}
+          id="rate"
+          value={[rate * 100]}
+          onValueChange={(val) => setRate(val[0] / 100)}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="flex flex-col w-full gap-4">
+        <Label htmlFor="rate" className="flex items-center justify-between">
+          <div>Speech Pitch</div>
+          <div>{pitch}x</div>
+        </Label>
+        <Slider
+          max={200}
+          step={1}
+          id="rate"
+          value={[pitch * 100]}
+          onValueChange={(val) => {
+            console.log(val[0] / 100);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            setPitch(val[0] / 100);
+          }}
+        />
       </div>
+
+      <Select
+        value={selectedVoiceId || undefined}
+        onValueChange={(val) => setSelectedVoiceId(val)}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="Select voice..." />
+        </SelectTrigger>
+        <SelectContent>
+          {voices.map((voice) => (
+            <SelectItem key={getVoiceId(voice)} value={getVoiceId(voice)}>
+              {`${voice.name} (${voice.lang})`}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      <Button
+        className="w-full"
+        disabled={!text || !selectedVoice}
+        onClick={isSpeaking ? stop : speak}
+      >
+        {isSpeaking ? "Stop" : "Play"}
+      </Button>
     </main>
   );
 }
